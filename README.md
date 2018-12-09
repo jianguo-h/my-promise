3de146d24f15f14653ef61724304631ccdffdeef
jianguo <1060684755@qq.com>
2018-12-09 16:55:32 +0800

    first step: 实现一个基本的promise

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# my-promise
-实现一个自己的Promise
+# 实现一个自己的Promise
+
+1. 实现一个基本的promise
+2. 实现then的链式调用
+3. 处理reolve函数的参数是promise实例的情况以及处理then方法中前一个回调函数返回的也是一个promise实例的情况
+4. 实现异步的promise
+5. 完善promise的其它方法
---
+++ index.js
@@ -0,0 +1,53 @@
+function MyPromise(fn) {
+  if (!(this instanceof MyPromise)) {
+    throw new TypeError('MyPromise must be constructed via new');
+  }
+  if (typeof fn !== 'function') {
+    throw new TypeError('MyPromise constructor argument is not a function');
+  }
+  this.state = 'pending';  // 出初始化状态
+  this.value = undefined;  // 初始化一个值, 用来存储resolve或者reject的值
+  // 执行 fn 方法
+  executeFn(fn, this);
+}
+
+MyPromise.prototype.then = function(onFullfilled, onRejected) {
+  var res = undefined;
+  var cb = this.state === 'resolved' ? onFullfilled : onRejected;
+  if(typeof cb !== 'function') {
+    cb = function(val) { return val; }
+  }
+  res = cb(this.value);
+}
+
+// 执行 fn 方法
+function executeFn(fn, promise) {
+  var done = false;     // 声明一个变量, 防止resolve, reject连续调用
+  try {
+    fn(function _resolve(value) {
+      if(done) return;
+      done = true;
+      resolve(promise, value);
+    }, function _reject(reason) {
+      if(done) return;
+      done = true;
+      reject(promise, reason);
+    });
+  }
+  catch(err) {
+    if(!done) {
+      done = true;
+      reject(promise, err);
+    }
+  }
+}
+
+function resolve(promise, value) {
+  promise.state = 'resolved';
+  promise.value = value;
+}
+
+function reject(promise, error) {
+  promise.state = 'rejected';
+  promise.value = error;
+}
